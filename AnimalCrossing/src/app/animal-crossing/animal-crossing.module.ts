e84394c1076675953aa56d9bcd42be6b2517b8e0
Laura <lauramorenoramos97@gmail.com>
2021-11-24 14:30:38 +0100

    Footer y navbar Implementados de base

---
+++ AnimalCrossing/src/app/animal-crossing/animal-crossing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AnimalCrossingRoutingModule } from './animal-crossing-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AnimalCrossingRoutingModule
+  ]
+})
+export class AnimalCrossingModule { }
